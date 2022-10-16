c669f29c350be088d8d973763b34e2629bfae782
Klenovszky András <klenovszky@gmail.com>
2022-10-16 20:51:19 +0200

    CurValues table populated

--- src/main/resources/scripts/query.sql
+++ src/main/resources/scripts/query.sql
@@ -1,11 +1,15 @@
 ----------------------------
 -- Application tables
 
-select * from survalues;
+select * from surValues;
 
 select * from outpay_header;
 
 select * from policy;
+
+truncate table survalues;
+truncate table outpay_header;
+truncate table policy;
 
 ----------------------------
 -- Liquibase tables
@@ -13,3 +17,9 @@ select * from policy;
 select * from databasechangelog;
 
 select * from databasechangeloglock;
+
+----------------------------
+-- Spring batch tables
+
+select * from batch_job_execution;
+select * from batch_step_execution;
